c19178d5bb9cb461fdf68ff034cfba3328d857c2
peanut7379 <peanut7379@gmail.com>
2017-10-17 01:28:52 +0800

    完善自动匹配分类信息

--- TutorialCrawler.java
+++ TutorialCrawler.java
@@ -12,19 +12,46 @@ import cn.edu.hfut.dmic.webcollector.plugin.berkeley.BreadthCrawler;
 import org.jsoup.select.Elements;
 import org.jsoup.nodes.Element;
 
+
+/**
+ * WebCollector 2.x版本的tutorial(2.20以上) 
+ * 2.x版本特性：
+ * 1）自定义遍历策略，可完成更为复杂的遍历业务，例如分页、AJAX
+ * 2）可以为每个URL设置附加信息(MetaData)，利用附加信息可以完成很多复杂业务，例如深度获取、锚文本获取、引用页面获取、POST参数传递、增量更新等。
+ * 3）使用插件机制，WebCollector内置两套插件。
+ * 4）内置一套基于内存的插件（RamCrawler)，不依赖文件系统或数据库，适合一次性爬取，例如实时爬取搜索引擎。
+ * 5）内置一套基于Berkeley DB（BreadthCrawler)的插件：适合处理长期和大量级的任务，并具有断点爬取功能，不会因为宕机、关闭导致数据丢失。 
+ * 6）集成selenium，可以对javascript生成信息进行抽取
+ * 7）可轻松自定义http请求，并内置多代理随机切换功能。 可通过定义http请求实现模拟登录。 
+ * 8）使用slf4j作为日志门面，可对接多种日志
+ *
+ * 可在cn.edu.hfut.dmic.webcollector.example包中找到例子(Demo)
+ *
+ * @author hu
+ */
 public class TutorialCrawler extends BreadthCrawler {
 
-
+    /*
+        该例子利用正则控制爬虫的遍历，
+        另一种常用遍历方法可参考DemoTypeCrawler
+    */
     
     public TutorialCrawler(String crawlPath, boolean autoParse) {
         super(crawlPath, autoParse);
         //start page
-        //addSeed("http://blog.csdn.net/");
+        //baidu助手
         //addSeed("https://mobile.baidu.com/list/soft/recommend");
-        addSeed("https://mobile.baidu.com/list/soft/board_507_102/507?ala=");
+        //应用宝
+        addSeed("http://sj.qq.com/myapp/category.htm?orgame=1");
+        //华为
+        addSeed("http://app.hicloud.com/soft/list");
 
-        //addRegex("http://blog.csdn.net/.*/article/details/.*");
-        addRegex("/list/soft/.*");
+
+
+        //应用宝
+        addRegex(".*orgame=1&categoryId=.*");
+        //华为
+        addRegex(".*/soft/list_.*");
         
         //需要抓取图片时设置为true，并加入图片的正则规则
         //setParseImg(true);
@@ -48,29 +75,43 @@ public class TutorialCrawler extends BreadthCrawler {
     */
     @Override
     public void visit(Page page, CrawlDatums next) {
-        if (page.matchUrl(".*/list/soft/.*")) {
+        if (page.matchUrl(".*/soft/list_.*")) {
             //String title = page.select(".applist li a[href *='item']").text();
-            Elements titles = page.select(".applist li a[href *='item']");
+
+            //baidu助手
+            //Elements titles = page.select(".applist li a[href *='item']");
+            //应用宝
+            //Elements titles = page.select(".app-info-desc a.name.ofh");
+            //华为
+            Elements titles = page.select("h4.title a");
+
             for(int i=0;i<titles.size();i++)
             {
                 System.out.println("title:" + titles.get(i).text()); //+ "\tauthor:" + author);
-                File file = new File("/Users/peanut/Downloads/output");
+                //应用宝分类名
+                //String id = page.url().substring(page.url().indexOf("Id=")+3);
+                //String category = page.select("li#cate-"+id).text();
+                //华为分类名
+                String category = page.select("span.key-select.txt-sml").text();
+
+
+                File file = new File("/Users/peanut/Downloads/huawei");
                 try{
                     if (!file.exists()) {
                         file.createNewFile();
                     }
                     FileWriter fileWriter = new FileWriter(file,true);
-                    fileWriter.write("办公学习\t"+ titles.get(i).text()+"\n");
+                    fileWriter.write(category+"\t"+ titles.get(i).text()+"\n");
                     fileWriter.close();}
                 catch(IOException exc){
                     System.out.println("Exception encountered: " + exc);
                 }
             }
 
-            Elements results = page.select("div .pagination >a");
-            Element result = results.get(0);
-            CrawlDatum datum = new CrawlDatum(result.attr("abs:href"));
-            next.add(datum);
+            //Elements results = page.select("div .pagination >a");
+            //Element result = results.get(0);
+            //CrawlDatum datum = new CrawlDatum(result.attr("abs:href"));
+            //next.add(datum);
         }
     }
 
